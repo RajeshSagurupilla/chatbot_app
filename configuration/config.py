9b33630beed0665a064cb8691a1d764925d3f11d
RajeshSagurupilla <sagurupillarajesh220@gmail.com>
2025-04-15 20:25:53 +0530

    added value for model keys

--- configuration/config.py
+++ configuration/config.py
@@ -1,6 +1,6 @@
 import os
 
 # Direct access
-OPENAI_API_KEY = os.getenv("OPENAI_API_KEY","")
-MIXTRAL_API_KEY = os.getenv("MIXTRAL_API_KEY",'')
+OPENAI_API_KEY = os.getenv("OPENAI_API_KEY","YOUR_API_KEY")
+MIXTRAL_API_KEY = os.getenv("MIXTRAL_API_KEY",'YOUR_API_KEY')
 MIXTRAL_API_URL = os.getenv("MIXTRAL_API_URL","https://router.huggingface.co/hf-inference/models/mistralai/Mixtral-8x7B-Instruct-v0.1/v1/chat/completions")
