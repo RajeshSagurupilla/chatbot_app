03c6b2d039a4028888a5e15480ae42874f4fe145
RajeshSagurupilla <sagurupillarajesh220@gmail.com>
2025-04-29 23:41:17 +0530

    removed URL

--- configuration/config.py
+++ configuration/config.py
@@ -3,4 +3,4 @@ import os
 # Direct access
 OPENAI_API_KEY = os.getenv("OPENAI_API_KEY","YOUR_API_KEY")
 MIXTRAL_API_KEY = os.getenv("MIXTRAL_API_KEY",'YOUR_API_KEY')
-MIXTRAL_API_URL = os.getenv("MIXTRAL_API_URL","https://router.huggingface.co/hf-inference/models/mistralai/Mixtral-8x7B-Instruct-v0.1/v1/chat/completions")
+MIXTRAL_API_URL = os.getenv("MIXTRAL_API_URL","YOUR_API_URL")
